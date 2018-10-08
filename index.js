d8564855fac8fd9286cd06ecc33035111fa27a32
Tomasz Harazin <tomasz.harazin@performgroup.com>
2018-10-08 14:00:24 +0200

    Add: basic structure

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => res.render('index', { title: 'Hey', message: 'Hello there!' }));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
